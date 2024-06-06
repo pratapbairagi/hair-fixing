06e98a98963d40c50f77a796f8f93181265f99a7
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2024-06-07 00:07:08 +0530

    changes css ftom gray to orange

--- src/components/services/services_card/service_card.jsx
+++ src/components/services/services_card/service_card.jsx
@@ -4,12 +4,12 @@
 const Service_card = ({img="", title="", details=""}) => {
     return (
         <>
-            <div className="min-w-[100%] lg:min-w-[20%] max-w-[300px] p-8 h-max shadow-sm hover:shadow-lg my-3">
+            <div className="min-w-[100%] lg:min-w-[20%] max-w-[300px] p-8 h-max shadow-sm hover:shadow-lg my-3 rounded-md">
                         <div className="service_icon">
                             <img src={img} className="w-16 border" alt="" />
                         </div>
-                        <h6 className="service_title text-gray-800 text-md font-bold text-left mt-4">{title}</h6>
-                        <p className="service_details line-clamp-3 text-xs text-gray-600 text-left mt-2"
+                        <h6 className="service_title text-orange-500 text-md font-bold text-left mt-4 capitalize">{title}</h6>
+                        <p className="service_details line-clamp-3 text-xs text-gray-500 text-left mt-2"
                         title={details}>
                         {details}
                         </p>
